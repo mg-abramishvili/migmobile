a8743fdfa809b667fd175d3032a987d70782c30e
Mikhail A <mg@abramishvili.net>
2022-06-05 13:20:35 +0500

    .

--- resources/views/order-confirmed.blade.php
+++ resources/views/order-confirmed.blade.php
@@ -4,12 +4,14 @@
 
 @section('content')
     <div class="order-page">
-    <h1 class="title-head mb-4">Заказ №{{ $order->id }} оплачен</h1>
-    <p>Ваш заказ:</p>
-    <ul>
-        @foreach($order->numbers as $number)
-            <li>{{ $number }}</li>
-        @endforeach
-    </ul>
+        <div class="container">
+            <h1 class="title-head mb-4">Заказ №{{ $order->id }} оплачен</h1>
+            <p>Ваш заказ:</p>
+            <ul>
+                @foreach($order->numbers as $number)
+                    <li>{{ $number }}</li>
+                @endforeach
+            </ul>
+        </div>
     </div>
 @endsection
