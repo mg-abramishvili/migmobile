06d63552e71039c65b0e41be233570d16b82078a
Mikhail A <mg@abramishvili.net>
2022-06-05 13:05:14 +0500

    .

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -10,4 +10,9 @@ class Setting extends Model
     use HasFactory;
 
     public $timestamps = false;
+
+    protected $fillable = [
+        'yookassa_shop_id',
+        'yookassa_secret_key'
+    ];
 }
