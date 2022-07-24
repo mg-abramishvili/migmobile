3cc9af0620aa2ca9cbdcef376dfba0af9b33257e
Mikhail Abramishvili <mg@abramishvili.net>
2022-07-24 18:00:54 +0500

    .

--- resources/js/components/front/CreateLead.vue
+++ resources/js/components/front/CreateLead.vue
@@ -51,17 +51,16 @@
         <div class="form-check">
             <input v-model="policy" class="form-check-input" type="checkbox" :id="'check_' + service">
             <label class="form-check-label" :for="'check_' + service">
-                <a href="/policy" target="_blank">
-                    <template v-if="lang == 'uz'">
-                        Шахсий маълумотларни қайта ишлашга розиман
-                    </template>
-                    <template v-else-if="lang == 'tj'">
-                        Ман барои коркарди маълумоти шахсӣ розӣ ҳастам
-                    </template>
-                    <template v-else>
-                        Согласен с обработкой персональных данных
-                    </template>
-                </a>
+                <template v-if="lang == 'uz'">
+                    Шахсий маълумотларни қайта ишлашга розиман
+                </template>
+                <template v-else-if="lang == 'tj'">
+                    Ман барои коркарди маълумоти шахсӣ розӣ ҳастам
+                </template>
+                <template v-else>
+                    Согласен с обработкой персональных данных
+                </template>
+                <a href="/policy" target="_blank">&rarr;</a>
             </label>
         </div>
         <button @click="save()" class="btn btn-primary">
