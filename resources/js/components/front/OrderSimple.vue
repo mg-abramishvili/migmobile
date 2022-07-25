2e623988d9d8c9144da754148b2f2cf111dd9c42
Mikhail Abramishvili <mg@abramishvili.net>
2022-07-25 12:41:34 +0500

    .

--- resources/js/components/front/OrderSimple.vue
+++ resources/js/components/front/OrderSimple.vue
@@ -18,12 +18,11 @@
                                 <h5 v-else class="card-title m-0">{{ plan.name_ru }}</h5>
                             </div>
                             <div class="card-plan-buy">
-                                <input v-model="selected.plans.find(p => p.name == plan.name).quantity" @change="saveCart()" type="number" min="0" :max="plan.in_stock" class="form-control">
-                                <button class="btn btn-primary">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
-                                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-                                    </svg>
-                                </button>
+                                <div class="input-group text-center">
+                                    <button @click="changeQuantityMinus(plan)" class="btn btn-primary btn-secondary">-</button>
+                                    <input v-model="selected.plans.find(p => p.name == plan.name).quantity" type="number" min="0" :max="plan.in_stock" disabled class="form-control fw-bold text-center">
+                                    <button @click="changeQuantityPlus(plan)" class="btn btn-primary">+</button>
+                                </div>
                             </div>
                         </div>
                     </template>
@@ -196,7 +195,25 @@ export default {
             
             this.saveCart()
         },
-        saveCart() {
+        changeQuantityMinus(plan) {
+            let selectedPlan = this.selected.plans.find(p => p.name == plan.name)
+            
+            if(selectedPlan.quantity > 0) {
+                selectedPlan.quantity = selectedPlan.quantity - 1
+            } else {
+                selectedPlan.quantity = 0
+            }
+        },
+        changeQuantityPlus(plan) {
+            let selectedPlan = this.selected.plans.find(p => p.name == plan.name)
+            
+            if(selectedPlan.quantity < plan.in_stock) {
+                selectedPlan.quantity = selectedPlan.quantity + 1
+            } else {
+                selectedPlan.quantity = plan.in_stock
+            }
+        },
+        saveCart(planQuantity, maxQuantity, event) {
             axios.post('/_simple-cart', {
                 simple_cart: { "plans": this.selected.plans, "price": this.priceWithQuantity }
             })
